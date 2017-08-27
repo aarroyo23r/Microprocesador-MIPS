dc6047ec08165140071d845e9ed8b8abb7dbb076
Emmanuel Araya Gamboa <emmanuelaraya114@gmail.com>
2017-08-27 17:24:45 -0600

    ajustes

--- Instruction_Fetch.v
+++ Instruction_Fetch.v
@@ -5,14 +5,12 @@ module Instruction_Fetch(
     input wire mux_ctrl,
     input wire [3:0]jp_address,
     output wire [31:0] instruction,
-    output reg [3:0] Pc_4
+    output reg [3:0] PC_4
     );
 
-reg [3:0] address=0;
+reg [3:0] address =4'h0;
 
-always@*begin
-Pc_4<=address+1;
-end
+
 
 always@(posedge clk)begin
 if(mux_ctrl == 1'b1)begin
@@ -22,6 +20,10 @@ else if(mux_ctrl==1'b0) begin
 end 
 
 
-RAM ram_unit(.clk(clk),.addr(address),.dataIn(0),.we(1'b0),.enable(1'b1),.reset(1'b0),.re(1'b1),.dataOut(instruction));
+RAM ram_unit(.clk(clk),.addr(address),.dataIn(),.we(1'b0),.enable(1'b1),.reset(1'b0),.re(1'b1),.dataOut(instruction));
+
+always@(posedge clk)begin
+PC_4<=address;
+end
     
 endmodule
