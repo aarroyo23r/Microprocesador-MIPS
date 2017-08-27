eb9bead42498dde6edda6b72143c160a1ab60423
Emmanuel Araya Gamboa <emmanuelaraya114@gmail.com>
2017-08-27 11:55:03 -0600

    IF

--- Instruction_Fetch.v
+++ Instruction_Fetch.v
@@ -4,26 +4,25 @@ module Instruction_Fetch(
     input wire clk,
     input wire mux_ctrl,
     input wire [3:0]jp_address,
-    output reg [31:0] instruction,
+    output wire [31:0] instruction,
     output reg [3:0] Pc_4
     );
 
-reg [3:0] address=4'h0;
-reg [3:0]PC=4'h0;
+reg [3:0] address=1;
 
 
 always@*begin
-if(mux_ctrl == 1'b0)begin
-    address=address+1; end
-else if(mux_ctrl == 1'b1)begin
-    address=jp_address;end
-else begin
-    address=address; end  
-end 
+Pc_4<=address+1;
+end
 
 always@(posedge clk)begin
-    PC<=address;end
+if(mux_ctrl == 1'b1)begin
+    address<=jp_address; end
+else begin
+    address<=address+1;end
+end 
 
-RAM ram_unit(.clk(clk),.addr(PC),.dataIn(),.we(1'b0),.enable(1'b1),.reset(1'b0),.re(1'b1),.dataOut(instruction));
+
+RAM ram_unit(.clk(clk),.addr(address),.dataIn(),.we(1'b0),.enable(1'b1),.reset(1'b0),.re(1'b1),.dataOut(instruction));
     
 endmodule
